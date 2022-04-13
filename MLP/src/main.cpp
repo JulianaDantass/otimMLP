acbb8a10dba6e9317445a03d669bd57ba3c9f894
JulianaDantass <julianadantas498@gmail.com>
2022-04-13 18:21:01 -0300

    update

--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -10,10 +10,26 @@
 using namespace std;
 
 double ** matrizAdj; // matriz de adjacencia
-int dimension; // quantidade total de vertices
+int vertices; // quantidade total de vertices
 
 
 void printData();
+
+struct Solution{
+  vector<int> sequence;
+};
+
+struct InsertionInfo{
+  int noInserido;
+  int arestaRemovida;
+  double custo;
+};
+
+struct SubsequenceInfo{
+  double tempoTotal;
+  double custoAcumulado;
+};
+
 
 struct Subsequence{
   double T, C;
@@ -37,24 +53,29 @@ struct Subsequence{
 
 };
 
-void UpdateAllSubseq(Solution *s, vector<vector<Subsequence>> &subseqMatrix){
+bool compares(InsertionInfo a, InsertionInfo b){  //funcao auxiliar na ordenacao dos custos
+  return a.custo < b.custo;
+}
 
-    int n= s->sequence.size();
+void updateSubseq(Solution &s, vector<vector<Subsequence>> &subseqMatrix){  //fazer o calculo do custo das subsequencias
+
+    int n= s.sequence.size();
 
     for(int i= 0; i < n; i++){
 
-      int v = s->sequence[i];
-      subseq_matrix[i][i].W = (i > 0);
-      subseq_matrix[i][i].C = 0;
-      subseq_matrix[i][i].T = 0;
-      subseq_matrix[i][i].first = s->sequence[i];
-      subseq_matrix[i][i].last = s->sequence[i];
+      int v = s.sequence[i];
+
+      subseqMatrix[i][i].W = (i > 0);
+      subseqMatrix[i][i].C = 0;
+      subseqMatrix[i][i].T = 0;
+      subseqMatrix[i][i].first = s.sequence[i];
+      subseqMatrix[i][i].last = s.sequence[i];
     }
 
     for (int i = 0; i < n; i++){
 
       for (int j = i + 1; j < n; j++){
-        subseq_matrix[i][j] = Subsequence::Concatenate(subseq_matrix[i][j-1], subseq_matrix[j][j]);
+        subseqMatrix[i][j] = Subsequence::Concatenate(subseqMatrix[i][j-1], subseqMatrix[j][j]);
       }
     
     }
@@ -62,9 +83,85 @@ void UpdateAllSubseq(Solution *s, vector<vector<Subsequence>> &subseqMatrix){
   for(int i= n-1; i >= 0; i--){
 
     for(int j= i-1; j >= 0; j--){
-      subseq_matrix[i][j] = Subsequence::Concatenate(subseq_matrix[i][j+1], subseq_matrix[j][j]);
+      subseqMatrix[i][j] = Subsequence::Concatenate(subseqMatrix[i][j+1], subseqMatrix[j][j]);
     }
   }
+}
+
+vector<InsertionInfo> calcularCustoInsercao (Solution& s, std::vector<int>& CL){  //calcular o custo da insercao de cada vertice para Construcao
+
+  std::vector<InsertionInfo> custoInsercao ((s.sequence.size()-1) * CL.size());
+
+  int a, b, i, k;
+  int count= 0;
+  int l= 0;
+  
+
+  for(int a= 0, b= 1; count < s.sequence.size() - 1; a++, b++){
+    
+    int i= s.sequence[a];
+    int j= s.sequence[b];
+
+    for (auto k : CL){
+      custoInsercao[l].custo= matrizAdj[i][k] + matrizAdj[j][k] - matrizAdj[i][j];
+      custoInsercao[l].noInserido= k;
+      custoInsercao[l].arestaRemovida= a;
+      l++;
+      
+    }
+    count++;
+  }
+  
+  return custoInsercao;
+}
+
+Solution Construction (Solution &s, vector<int> CL){  //metodo da construcao
+
+  int indexRandom, i, j;  
+  
+  s.sequence.push_back(1);
+  s.sequence.push_back(1);      //adicionando a c1 no inicio e no final
+
+
+  //escolher 3 cidades aleatorias
+      
+    indexRandom= rand() % CL.size();
+    s.sequence.insert(s.sequence.end()-1, CL[indexRandom]);
+    CL.erase(CL.begin() + indexRandom);
+
+    indexRandom= rand() % CL.size();
+    s.sequence.insert(s.sequence.end()-1, CL[indexRandom]);
+    CL.erase(CL.begin() + indexRandom);
+
+    indexRandom= rand() % CL.size();
+    s.sequence.insert(s.sequence.end()-1, CL[indexRandom]);
+    CL.erase(CL.begin() + indexRandom);
+
+    //
+
+  while(!CL.empty()){
+
+    std::vector<InsertionInfo> custoInsercao= calcularCustoInsercao(s, CL);
+
+    sort(custoInsercao.begin(), custoInsercao.end(), compares);   
+    
+    double alpha= (double) rand() / RAND_MAX;
+    int selecionado= rand() % ( (int) ceil(alpha * custoInsercao.size()) );
+
+    s.sequence.insert(s.sequence.begin()+ custoInsercao[selecionado].arestaRemovida + 1, custoInsercao[selecionado].noInserido);
+
+    j= 0;
+    while(1){                     //retirando a cidade inserida da lista de candidatos  
+      if(custoInsercao[selecionado].noInserido == CL[j]){
+        CL.erase(CL.begin()+j);
+        break;
+      }
+      j++;
+    }
+
+  }
+
+  return s;
 }
 
 
@@ -77,56 +174,57 @@ int main(int argc, char** argv) {
     int maxIter, maxIterIls;
     int i, count, j;
     vector<int> CL;    
-    vector<int> CL2; 
+    vector<vector<Subsequence>> subseqMatrix;
 
-    readData(argc, argv, &dimension, &matrizAdj);
+    readData(argc, argv, &vertices, &matrizAdj);
 
+   
 
-    if(dimension <= 150){
-      maxIterIls= dimension/ 2.0;
+    if(vertices <= 150){
+      maxIterIls= vertices/ 2.0;
     }else{
-      maxIterIls= dimension;
+      maxIterIls= vertices;
     }
     srand(time(NULL));
 
-    for(i= 2; i <= dimension; i++){ //comeca do 2 pq a c1 é adicionada na s.sequence na construcao
+    for(i= 2; i <= vertices; i++){ 
       CL.push_back(i);
     }
 
-   bestOfAll.custoSolucao= (double) INFINITY;
+   bestOfAll.costSolution= (double) INFINITY;
    maxIter= 50;
    for(i= 0; i < maxIter; i++){
       
+      s= Construction(s, CL);
+
+      updateSubseq(s, subseqMatrix);
+
+      // bestS= s;
+
+      // count= 0;
       
-      CL2= CL;
-      s= Construcao(CL2);
 
-      bestS= s;
-
-      count= 0;
-      
-
-      while(count < maxIterIls){
-        BuscaLocal(s);
+      // // while(count < maxIterIls){
+      // //   BuscaLocal(s);
         
-        if(s.custoSolucao < bestS.custoSolucao){
-          bestS= s;
-          count= 0;
-        }
-        // for(j= 0; j < s.sequence.size(); j++){
-        //   cout << s.sequence[j] << " ";
-        // }
-        // cout << endl;
-          s= Pertubacao(bestS);  
+      // //   if(s.costSolution < bestS.costSolution){
+      // //     bestS= s;
+      // //     count= 0;
+      // //   }
+      // //   // for(j= 0; j < s.sequence.size(); j++){
+      // //   //   cout << s.sequence[j] << " ";
+      // //   // }
+      // //   // cout << endl;
+      // //     s= Pertubacao(bestS);  
          
-        // for(j= 0; j < s.sequence.size(); j++){
-        //   cout << s.sequence[j] << " ";
-        // }
-        // cout << s.custoSolucao << endl;
-        count++;
-      }
+      // //   // for(j= 0; j < s.sequence.size(); j++){
+      // //   //   cout << s.sequence[j] << " ";
+      // //   // }
+      // //   // cout << s.custoSolucao << endl;
+      // //   count++;
+      // // }
 
-    if(bestS.custoSolucao < bestOfAll.custoSolucao){
+    if(bestS.costSolution < bestOfAll.costSolution){
       bestOfAll= bestS;
     }
      
